e2cf9241a017981661779bec229bce0888c5e9fb
yasinadil <adilyasin205@gmail.com>
2023-06-25 02:04:03 +0500

    frontend changes

--- components/SpaceCard/SpaceCard.tsx
+++ components/SpaceCard/SpaceCard.tsx
@@ -3,8 +3,10 @@ import React from "react";
 import Image from "next/image";
 import space from "../../public/assets/space.png";
 import Link from "next/link";
-import polygon from "../../public/assets/polygon.png";
-import offer from "../../public/assets/offer.png";
+import grayBalance from "../../public/assets/grayBalance.png";
+import purpleBalance from "../../public/assets/purpleBalance.png";
+import grayOffer from "../../public/assets/gray-offer.png";
+import purpleOffer from "../../public/assets/purple-offer.png";
 import { ethers } from "ethers";
 
 export default async function SpaceCard(props: any) {
@@ -33,28 +35,67 @@ export default async function SpaceCard(props: any) {
         </Link>
       </figure>
       <div className="card-body">
-        <h2 className="card-title">
-          <Link href={`/space/${props.id}`}>
-            Space #{props.id < 10 && "0"}
-            {props.id}
-          </Link>
-          <div className="badge badge-primary">
-            {props.exists ? "CLAIMED" : "UNCLAIMED"}
+        <div className="flex flex-row justify-around items-center">
+          <div className="flex justify-around items-center gap-2">
+            <h2 className="card-title">
+              <Link href={`/space/${props.id}`}>
+                #{props.id < 10 && "0"}
+                {props.id}
+              </Link>
+            </h2>
+            <div className="badge badge-primary">
+              {props.exists ? "CLAIMED" : "UNCLAIMED"}
+            </div>
           </div>
-        </h2>
 
-        <div className="card-actions justify-end pt-2">
-          <div className="badge border-0 tooltip flex" data-tip="offer">
-            <Image className="w-7 pr-1" src={offer} alt="offer" />
-            {ethers.utils.formatEther(props.highestOffer.toString())}
-          </div>
-          <div className="badge border-0 tooltip flex" data-tip="balance">
-            <Image className="w-7 pr-1" src={polygon} alt="balance" />
-            {Number(props.balance) > 0
-              ? Number(
-                  ethers.utils.formatEther(props.balance.toString())
-                ).toFixed(8)
-              : ethers.utils.formatEther(props.balance.toString())}
+          <div className="flex justify-between items-center">
+            <div
+              className="badge border-0 tooltip flex"
+              data-tip={`${ethers.utils.formatEther(
+                props.highestOffer.toString()
+              )}`}
+            >
+              {Number(props.highestOffer) > 0 ? (
+                <Image
+                  className="w-7 pr-1"
+                  src={purpleOffer}
+                  alt={`${ethers.utils.formatEther(
+                    props.highestOffer.toString()
+                  )}`}
+                />
+              ) : (
+                <Image
+                  className="w-7 pr-1"
+                  src={grayOffer}
+                  alt={`${ethers.utils.formatEther(
+                    props.highestOffer.toString()
+                  )}`}
+                />
+              )}
+              {/* <Image
+              className="w-7 pr-1"
+              src={grayOffer}
+              alt={`${ethers.utils.formatEther(props.highestOffer.toString())}`}
+            /> */}
+              {/* {ethers.utils.formatEther(props.highestOffer.toString())} */}
+            </div>
+
+            <div
+              className="badge border-0 tooltip flex"
+              data-tip={`${
+                Number(props.balance) > 0
+                  ? Number(
+                      ethers.utils.formatEther(props.balance.toString())
+                    ).toFixed(8)
+                  : ethers.utils.formatEther(props.balance.toString())
+              }`}
+            >
+              {Number(props.balance) > 0 ? (
+                <Image className="w-7 pr-1" src={purpleBalance} alt="balance" />
+              ) : (
+                <Image className="w-7 pr-1" src={grayBalance} alt="balance" />
+              )}
+            </div>
           </div>
         </div>
       </div>
